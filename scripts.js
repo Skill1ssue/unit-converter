618057ba0c2530d5763171e2bb8b4819856ef0b8
Skill1ssue <berlinguzman01@gmail.com>
2024-10-16 13:09:24 -0400

    upload: new features

--- scripts.js
+++ scripts.js
@@ -4,9 +4,11 @@ const inputBtn = document.getElementById("input-btn")
 const length = document.getElementById("length")
 const volume = document.getElementById("volume")
 const mass = document.getElementById("mass")
+const footer = document.getElementById("footer")
 const meterToFeet = 3.281
 const literToGallon = 0.264
 const kilogramToPound = 2.204
+const iphone = 16.07
 
 // Event Listener
 inputBtn.addEventListener("click", () => renderResults(Number(inputEl.value)))
@@ -33,8 +35,16 @@ function getMass(input) {
   return massArr
 }
 
+function getIphoneLength(input) {
+  let iphoneLengthArr = []
+  iphoneLengthArr.push(Math.floor(input * iphone))
+  iphoneLengthArr.push(Math.floor(input / iphone))
+  return iphoneLengthArr
+}
+
 function renderResults(input) {
   length.innerText = `${input} meters = ${getLength(input)[0]} feet | ${input} feet = ${getLength(input)[1]} meters`
   volume.innerText = `${input} liters = ${getVolume(input)[0]} gallons | ${input} gallons = ${getVolume(input)[1]} liters`
   mass.innerText = `${input} kilos = ${getMass(input)[0]} pounds | ${input} pounds = ${getMass(input)[1]} kilos`
+  footer.innerHTML = `Or the equivalent of ${getIphoneLength(input)[0]} Iphones 16 Pro Max (for my friends in the USA) <span>😂</span>`
 }
